f7af50c2c57eb6762075573aa0235dc36da4e054
Vladislav Gavrish <vdgavrish@gmail.com>
2020-03-20 17:03:39 +0300

    added modal

--- src/js/script.js
+++ src/js/script.js
@@ -84,4 +84,29 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   setClock('timer', deadline);
+
+  //Modal
+
+  let more = document.querySelector('.more'),
+      overlay = document.querySelector('.overlay'),
+      close = document.querySelector('.popup-close'),
+      descriptionBtn = document.querySelectorAll('.description-btn');
+
+  more.addEventListener('click', showOverlay);
+  
+  for(let i = 0; i < descriptionBtn.length; i++) {
+    descriptionBtn[i].addEventListener('click', showOverlay);
+  }
+
+  function showOverlay() {
+    overlay.style.display = 'block';
+    more.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  }
+
+  close.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  });
 });
