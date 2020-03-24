a65dd367751cc2c093ae7147b347994d7c0b70d1
Vladislav Gavrish <vdgavrish@gmail.com>
2020-03-24 10:56:43 +0300

    added form

--- src/js/script.js
+++ src/js/script.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
   //Timer
 
-  let deadline = '2020-03-21';
+  let deadline = '2020-03-25';
 
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -108,5 +108,51 @@ window.addEventListener('DOMContentLoaded', function() {
     overlay.style.display = 'none';
     more.classList.remove('more-splash');
     document.body.style.overflow = '';
+  });
+
+  // Form
+  let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так'
+  };
+
+  let form = document.querySelector('.main-form'),
+      input = form.getElementsByTagName('input'),
+      statusMessage = document.createElement('div');
+
+      statusMessage.classList.add('status');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+    let formData = new FormData(form);
+
+    let obj = {};
+    formData.forEach(function(value, key) {
+      obj[key] = value;
+    });
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', function() {
+      if(request.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+      } else if(request.readyState === 4 && request.status == 200) {
+        statusMessage.innerHTML = message.success;
+      } else {
+        statusMessage.innerHTML = message.failure;
+      }
+    }); 
+
+    for(let i = 0; i < input.length; i++) {
+      input[i].value = '';
+    }
   });
 });
