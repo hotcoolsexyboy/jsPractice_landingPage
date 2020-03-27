dcfa4ec188a12b4e82680c6d117a902b53a3e1a9
Vladislav Gavrish <vdgavrish@gmail.com>
2020-03-27 08:01:19 +0300

    added promises

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,31 @@
-window.addEventListener('DOMContentLoaded', function() {
-  'use strict';
+window.addEventListener("DOMContentLoaded", function() {
+  "use strict";
 
-  let tab = document.querySelectorAll('.info-header-tab'),
-      info = document.querySelector('.info-header'),
-      tabContent  = document.querySelectorAll('.info-tabcontent');
+  let tab = document.querySelectorAll(".info-header-tab"),
+    info = document.querySelector(".info-header"),
+    tabContent = document.querySelectorAll(".info-tabcontent");
 
   function hideTabContent(a) {
-    for(let i = a; i < tabContent.length; i++) {
-      tabContent[i].classList.remove('show');
-      tabContent[i].classList.add('hide');
+    for (let i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove("show");
+      tabContent[i].classList.add("hide");
     }
   }
 
   hideTabContent(1);
 
   function showTabContent(b) {
-    if(tabContent[b].classList.contains('hide')) {
-      tabContent[b].classList.remove('hide');
-      tabContent[b].classList.add('show');
+    if (tabContent[b].classList.contains("hide")) {
+      tabContent[b].classList.remove("hide");
+      tabContent[b].classList.add("show");
     }
   }
 
-  info.addEventListener('click', function(event) {
+  info.addEventListener("click", function(event) {
     let target = event.target;
-    if(target && target.classList.contains('info-header-tab')) {
-      for(let i = 0; i < tab.length; i++) {
-        if(target == tab[i]) {
+    if (target && target.classList.contains("info-header-tab")) {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
           hideTabContent(0);
           showTabContent(i);
           break;
@@ -36,37 +36,37 @@ window.addEventListener('DOMContentLoaded', function() {
 
   //Timer
 
- //let deadline = '2020-03-25';
+  //let deadline = '2020-03-25';
   let tomorrow = Date.parse(new Date()) + 8640000,
-      deadline = tomorrow;
+    deadline = tomorrow;
 
   function getTimeRemaining(endtime) {
     let t = endtime - Date.parse(new Date()),
-        seconds = Math.floor((t/1000) % 60),
-        minutes = Math.floor((t/1000/60) % 60),
-        hours = Math.floor((t/(1000*60*60)));
-    
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor(t / (1000 * 60 * 60));
+
     return {
-      'total' : t,
-      'seconds' : seconds,
-      'minutes' : minutes,
-      'hours' : hours
+      total: t,
+      seconds: seconds,
+      minutes: minutes,
+      hours: hours
     };
   }
 
   function setClock(id, endtime) {
     let timer = document.getElementById(id),
-        hours = timer.querySelector('.hours'),
-        minutes = timer.querySelector('.minutes'),
-        seconds = timer.querySelector('.seconds'),
-        timeInterval = setInterval(updateClock, 1000);
+      hours = timer.querySelector(".hours"),
+      minutes = timer.querySelector(".minutes"),
+      seconds = timer.querySelector(".seconds"),
+      timeInterval = setInterval(updateClock, 1000);
 
     function updateClock() {
       let t = getTimeRemaining(endtime);
 
       function addZero(num) {
-        if(num <= 9) {
-          return '0' + num;
+        if (num <= 9) {
+          return "0" + num;
         } else {
           return num;
         }
@@ -76,86 +76,100 @@ window.addEventListener('DOMContentLoaded', function() {
       minutes.textContent = addZero(t.minutes);
       seconds.textContent = addZero(t.seconds);
 
-      if(t.total <= 0) {
+      if (t.total <= 0) {
         clearInterval(timeInterval);
-        hours.textContent = '00';
-        minutes.textContent = '00';
-        seconds.textContent = '00';
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        seconds.textContent = "00";
       }
     }
   }
 
-  setClock('timer', deadline);
+  setClock("timer", deadline);
 
   //Modal
 
-  let more = document.querySelector('.more'),
-      overlay = document.querySelector('.overlay'),
-      close = document.querySelector('.popup-close'),
-      descriptionBtn = document.querySelectorAll('.description-btn');
+  let more = document.querySelector(".more"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close"),
+    descriptionBtn = document.querySelectorAll(".description-btn");
 
-  more.addEventListener('click', showOverlay);
-  
-  for(let i = 0; i < descriptionBtn.length; i++) {
-    descriptionBtn[i].addEventListener('click', showOverlay);
+  more.addEventListener("click", showOverlay);
+
+  for (let i = 0; i < descriptionBtn.length; i++) {
+    descriptionBtn[i].addEventListener("click", showOverlay);
   }
 
   function showOverlay() {
-    overlay.style.display = 'block';
-    more.classList.add('more-splash');
-    document.body.style.overflow = 'hidden';
+    overlay.style.display = "block";
+    more.classList.add("more-splash");
+    document.body.style.overflow = "hidden";
   }
 
-  close.addEventListener('click', function() {
-    overlay.style.display = 'none';
-    more.classList.remove('more-splash');
-    document.body.style.overflow = '';
+  close.addEventListener("click", function() {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
   });
 
-  // Form
+  //Form
+
   let message = {
-    loading: 'Загрузка...',
-    success: 'Спасибо! Скоро мы с вами свяжемся!',
-    failure: 'Что-то пошло не так'
+    loading: "Загрузка",
+    success: "Спасибо! Мы с вами свяжемся!",
+    failure: "Ошибка..."
   };
-  function sendForm(name) {
-    let form = document.querySelector(name),
-    input = form.getElementsByTagName('input'),
-    statusMessage = document.createElement('div');
 
-    statusMessage.classList.add('status');
+  function postData(selectorName) {
+    let form = document.querySelector(selectorName),
+      input = form.getElementsByTagName("input"),
+      statusMessage = document.createElement("div");
 
-    form.onsubmit = function(event) {
+    statusMessage.classList.add("status");
+
+    form.addEventListener("submit", function(event) {
       event.preventDefault();
       form.appendChild(statusMessage);
 
       let request = new XMLHttpRequest();
-      request.open('POST', 'server.php');
-      request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+      request.open("POST", "server.php");
+      request.setRequestHeader(
+        "Content-type",
+        "application/json; charset=utf-8"
+      );
 
       let formData = new FormData(form);
+
       let obj = {};
       formData.forEach(function(value, key) {
         obj[key] = value;
       });
       let json = JSON.stringify(obj);
+
       request.send(json);
 
-      request.onreadystatechange = function() {
-        if(request.readyState < 4) {
-          statusMessage.innerHTML = message.loading;
-        } else if(request.readyState === 4 && request.status == 200) {
-          statusMessage.innerHTML = message.success;
-        } else {
-          statusMessage.innerHTML = message.failure;
-        }
-      };
+      request.addEventListener("readystatechange", function() {
+        let promise = new Promise(function(resolve, reject) {
+          if (request.readyState < 4) {
+            resolve();
+          } else if (request.readyState === 4 && request.status == 200) {
+            resolve();
+          } else {
+            reject();
+          }
+        });
+        promise
+          .then(() => (statusMessage.innerHTML = message.loading))
+          .then(() => (statusMessage.innerHTML = message.success))
+          .catch(() => (statusMessage.innerHTML = message.failure));
+      });
 
-      for(let i = 0; i < input.length; i++) {
-        input[i].value = '';
+      for (let i = 0; i < input.length; i++) {
+        input[i].value = "";
       }
-    };
+    });
   }
-  sendForm('.main-form');
-  sendForm('#form');
+
+  postData('.main-form');
+  postData('#form');
 });
