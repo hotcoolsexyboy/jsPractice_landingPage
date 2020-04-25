0df76c2e1ec4e0263f8369c096aaac030bc96ac3
Vladislav Gavrish <vdgavrish@gmail.com>
2020-04-25 15:46:01 +0300

    fixed promises

--- src/js/parts/forms.js
+++ src/js/parts/forms.js
@@ -16,25 +16,25 @@ function forms() {
       event.preventDefault();
       form.appendChild(statusMessage);
 
-      let request = new XMLHttpRequest();
-      request.open("POST", "server.php");
-      request.setRequestHeader(
-        "Content-type",
-        "application/json; charset=utf-8"
-      );
+      let promise = new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
+        request.open("POST", "server.php");
+        request.setRequestHeader(
+          "Content-type",
+          "application/json; charset=utf-8"
+        );
 
-      let formData = new FormData(form);
+        let formData = new FormData(form);
 
-      let obj = {};
-      formData.forEach(function (value, key) {
-        obj[key] = value;
-      });
-      let json = JSON.stringify(obj);
+        let obj = {};
+        formData.forEach(function (value, key) {
+          obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
 
-      request.send(json);
+        request.send(json);
 
-      request.addEventListener("readystatechange", function () {
-        let promise = new Promise(function (resolve, reject) {
+        request.addEventListener("readystatechange", function () {
           if (request.readyState < 4) {
             resolve();
           } else if (request.readyState === 4 && request.status == 200) {
@@ -43,11 +43,12 @@ function forms() {
             reject();
           }
         });
-        promise
-          .then(() => (statusMessage.innerHTML = message.loading))
-          .then(() => (statusMessage.innerHTML = message.success))
-          .catch(() => (statusMessage.innerHTML = message.failure));
       });
+
+      promise
+        .then(() => (statusMessage.innerHTML = message.loading))
+        .then(() => (statusMessage.innerHTML = message.success))
+        .catch(() => (statusMessage.innerHTML = message.failure));
 
       for (let i = 0; i < input.length; i++) {
         input[i].value = "";
